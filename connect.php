19a0de30123d709610e78b58bf2665525f6b1e6e
instance creation <sourabh.purohit1857@gmail.com>
2020-05-20 20:42:30 +0530

    This code are use for connect ec2 instace to db instacne (db instance)

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php 
+$username = "sourabhtesting"; 
+$password = "sourabhtesting@123"; 
+$hostname = "yourhostnameaddress"; 
+$dbname = "sourabhtesting";
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL"); 
+echo "Connected to MySQL using username - $username, password - $password, host - $hostname<br>"; 
+$selected = mysql_select_db("$dbname",$dbhandle)   or die("Unable to connect to MySQL DB - check the database name and try again."); 
+?>
